c85d8a1a7e19f57e29e2bffbc624fe6d6fe83f83
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2023-04-15 15:10:38 +0200

    Add optional notifications

--- aswp.sh
+++ aswp.sh
@@ -79,29 +79,35 @@ function swap() {
     if [[ "$CUR_SINK" == "$SINK1" && "$CUR_PORT" == "$PORT1" ]] ; then
         NEW_SINK="$SINK2"
         NEW_PORT="$PORT2"
+        NEW_NAME="$NAME2"
     else
         NEW_SINK="$SINK1"
         NEW_PORT="$PORT1"
+        NEW_NAME="$NAME1"
     fi
 
     TAB=`printf '\t'`
-    echo "Swapping to:
-${TAB}Sink: $NEW_SINK
-${TAB}Port: $NEW_PORT"
+    echo "Swapping to: $NEW_NAME"
 
     pactl set-default-sink "$NEW_SINK" &&
         pactl set-sink-port "$NEW_SINK" "$NEW_PORT"
+    RES=$?
+
+    if [[ "$NOTIFY" == "yes" ]] ; then
+        notify-send -a aswp -t "$N_LEN" "Now output in $NEW_NAME"
+    fi
 }
 
 function config() {
-    echo "This will set up aswp. You need a way to set your audio outputs
+    printf "This will set up aswp. You need a way to set your audio outputs
 while this script is running. To cancel the process you can press Ctrl+C
 The first audio output you set will be the default to change to if neither of
 the two devices is selected.
 
-To continue, change your default audio output to the first device and press
-enter:"
-    read
+To continue, change your default audio output to the first device and enter its
+name (how would you like the device to be reffered to e.g. headphones):
+ > "
+    NAME1=`head -1`
 
     get_current_device
     SINK1="$CUR_SINK"
@@ -109,11 +115,13 @@ enter:"
     echo "Device 1:
     Sink: $SINK1
     Port: $PORT1
+    Name: '$NAME1'
 "
 
-    echo "Now change the default audio output to the second device and press
-enter:"
-    read
+    printf "Now change the default audio output to the second device and enter
+its name (how would you like the device to be reffered to e.g. speakers):
+ > "
+    NAME2=`head -1`
 
     get_current_device
     SINK2="$CUR_SINK"
@@ -121,10 +129,21 @@ enter:"
     echo "Device 2:
     Sink: $SINK2
     Port: $PORT2
+    Name: '$NAME2'
 "
 
-    echo "Press enter now to write the settings to the configuration file
-'$CONFIG_FILE'. All contets of that file will be overwritten:"
+    printf "Would you like to push notification every time you swap your
+device? [yes/<other>]
+ > "
+    NOTIFY=`head -1`
+    if [[ "$NOTIFY" != 'yes' ]] ; then
+        NOTIFY=no
+    fi
+
+    echo "
+Press enter now to write the settings to the configuration file
+'$CONFIG_FILE'. All contets of that file will be overwritten. You can edit the
+file any time:"
 
     read
 
@@ -136,12 +155,21 @@ enter:"
 # this script is run every time the aswp script is run
 # this file is generated again every time 'awp config' is run
 
+# valid values are case sensitive [yes/no]
+NOTIFY='$NOTIFY'
+
+# how long to show notifications (in ms)
+N_LEN='1000'
+
 # devices:
 SINK1='$SINK1'
 PORT1='$PORT1'
+NAME1='$NAME1'
 
 SINK2='$SINK2'
-PORT2='$PORT2'" > "$CONFIG_FILE"
+PORT2='$PORT2'
+NAME2='$NAME2'
+" > "$CONFIG_FILE"
 }
 
 # prints the help
@@ -177,15 +205,36 @@ ${GREEN}Usage:$RESET
   ${WHITE}aswp help$RESET
     Shows this help
 
+  ${WHITE}aswp notify$RESET
+    Shows notification with the current audio device
+
 ${GREEN}Options:
   $YELLOW-h  --help  -?$RESET
     shows this help"
+}
+
+function notify() {
+    # the config file is just script with the variables
+    . $CONFIG_FILE || exit 1
+
+    get_current_device
+
+    if [[ "$CUR_SINK" == "$SINK1" && "$CUR_PORT" == "$PORT1" ]] ; then
+        CUR_NAME="$NAME1"
+    elif [[ "$CUR_SINK" == "$SINK2" && "$CUR_PORT" == "$PORT2" ]] ; then
+        CUR_NAME="$NAME2"
+    else
+        CUR_NAME="$CUR_SINK - $CUR_PORT"
+    fi
+
+    notify-send -a aswp -t $N_LEN "$CUR_NAME"
 }
 
 case "$1" in
 "") swap ;;
 config) config ;;
 help|-h|--help|-\?) help_fun ;;
+notify) notify ;;
 *)  echo "Invalid argument"
     exit 1
     ;;
